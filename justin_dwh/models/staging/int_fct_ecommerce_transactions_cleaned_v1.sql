f6afa61672966412632cf7d25d7a2aa25e869e07
Justin De Guia <justin@insight.co>
2024-02-18 22:51:04 +0800

    change stg to int (#11)

---
+++ justin_dwh/models/staging/int_fct_ecommerce_transactions_cleaned_v1.sql
@@ -0,0 +1,62 @@
+{{ 
+  config(
+    materialized='ephemeral'
+  )
+}}
+
+
+WITH remove_missing_customer_id AS (
+    SELECT * EXCLUDE (InvoiceDate),
+        CAST(InvoiceDate AS DATE) AS InvoiceDate,
+    FROM ecommerce_transactions
+    WHERE CustomerID IS NOT NULL
+    
+    {%- if is_incremental() %}
+    AND CAST(InvoiceDate AS DATE) BETWEEN CAST('{{ var("from_date") }}' AS DATE) AND CAST('{{ var("to_date") }}' AS DATE)
+    {%- endif %}
+),
+
+remove_null_description AS (
+    SELECT *
+    FROM remove_missing_customer_id
+    WHERE Description IS NOT NULL
+),
+
+remove_weird_stockcodes AS (
+    SELECT *
+    FROM remove_null_description
+    WHERE StockCode NOT IN (
+        'POST', 
+        'D', 
+        'C2', 
+        'M', 
+        'BANK CHARGES', 
+        'PADS', 
+        'DOT', 
+        'CRUK',
+        '23444',
+        '23702')
+),
+
+add_product_status AS (
+    SELECT *,
+    CASE 
+        WHEN Quantity > 1 THEN 'Completed'
+        ELSE 'Cancelled'
+    END AS TransactionStatus
+    FROM remove_weird_stockcodes
+),
+
+remove_free_products AS (
+    SELECT * 
+    FROM add_product_status
+    WHERE UnitPrice > 0
+),
+
+latest_transaction AS (
+    SELECT *,
+    row_number() OVER (PARTITION BY CustomerID, InvoiceNo, StockCode ORDER BY InvoiceDate DESC) AS row_num
+    FROM remove_free_products   
+)
+
+SELECT * FROM latest_transaction WHERE row_num = 1
